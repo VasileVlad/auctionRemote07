6ac24e4f752ae8019fc7acba3f457a12d93a5706
VasileVlad <vasilevladutz@gmail.com>
2021-02-14 09:31:32 +0200

    add bootstrap, security...

--- src/main/java/com/sda/auction/controller/HomeController.java
+++ src/main/java/com/sda/auction/controller/HomeController.java
@@ -1,13 +1,26 @@
 package com.sda.auction.controller;
 
 import com.sda.auction.dto.UserDto;
+import com.sda.auction.service.UserService;
+import com.sda.auction.validator.UserDtoValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
+
+    private UserService userService;
+    private UserDtoValidator userDtoValidator;
+
+    @Autowired
+    public HomeController(UserService userService, UserDtoValidator userDtoValidator) {
+        this.userService = userService;
+        this.userDtoValidator = userDtoValidator;
+    }
 
     @GetMapping("/home")
     public String getHomepage(){
@@ -22,8 +35,15 @@ public class HomeController {
     }
 
     @PostMapping("/register")
-    public String postRegisterPage(UserDto userDto){
-        System.out.println("Se apeleaza post register cu" + userDto);
-        return "register";
+    public String postRegisterPage(Model model, UserDto userDto, BindingResult bindingResult){
+//        System.out.println("Se apeleaza post register cu" + userDto);
+        userDtoValidator.validate(userDto, bindingResult);
+        if (bindingResult.hasErrors()){
+            model.addAttribute("userDto", userDto);
+            return "register";
+        }
+        userService.register(userDto);
+        return "redirect:/home";
     }
+
 }
---
+++ src/main/java/com/sda/auction/model/Role.java
@@ -0,0 +1,20 @@
+package com.sda.auction.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "role")
+    private List<User> users;
+}
